1fdde5f4b81cc5192c153a0573e6d8f1eb190b75
ShubhamSingh047 <shubham.shubham.singh13@gmail.com>
2022-11-02 18:17:50 +0530

    validation

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,47 +1,45 @@
 import React, { useState } from "react";
 
-
 const FormExampleForm = () => {
  
   const [form, setForm] = useState({
     email: "",
   });
 
-  const [errMsg, setErrMsg] = useState( );
-
+  const [errMsg, setErrMsg] = useState(null);
 
   const onSubmit = (e) => {
     const { email } = e.target;
- 
-    const emailCharFormat =
-      /^[A-z0-9\u0000-\u007f\s'.,-/@#!$%^&*;:{}=\-_`~()]+$/;
-    if (email?.value && !emailCharFormat.test(email.value)) {
-      setErrMsg( "We don't allow any special characters in the customers email and ID.")
-      
-    }
     const emailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
-    if (email?.value && !emailFormat.test(email.value)) {
-      setErrMsg( "Please enter your email in this format: name@company.com.");
+    const emailCharFormat =/^[A-z0-9\u0000-\u007f\s'.,-/@#!$%^&*;:{}=\-_`~()]+$/;
+    if(email?.value==""){
+      setErrMsg("Please enter your email in this format: name@company.com.");
     }
-    if( email?.value && emailCharFormat.test(email.value) && emailFormat.test(email.value)){
-      setErrMsg ("Email is valid");
+    if (email?.value && !emailCharFormat.test(email.value)) {
+      setErrMsg( "We don't allow any special characters in the customers email and ID.");
     }
+    else{
+      if (email?.value && !emailFormat.test(email.value)) {
+        setErrMsg( "Please enter your email in this format: name@company.com.");
+      }
+      else if( email?.value && emailCharFormat.test(email.value) && emailFormat.test(email.value)){
+        setErrMsg ("Email is valid");
+      }
+  }
     e.preventDefault();
-    console.log(e, " even and state ", form);
   };
 
   return (
     <>
-      <form onSubmit={onSubmit} className="container-form">
+      <form onSubmit={onSubmit} className="container-form" noValidate>
         <div className="field">
           <label className="field-label" htmlFor="name">
             Email :{" "}
           </label>
-          <input className="field-input" type="email" id="name" name="email" />
+          <input className="field-input" type="email" id="name" name="email" data-testid="inputTestId" />
         </div>
-
+        <p data-testid="submitTestIdPara" style={errMsg!="Email is valid"?{color:"red"}:{color:"green"}}>{errMsg}</p>
         <input className="form-button" type="submit" value="Submit" data-testid="submitTestId" />
-        <p data-testid="submitTestIdPara" > {errMsg}</p>
       </form>
     </>
   );
--- src/Form/Form.test.tsx
+++ src/Form/Form.test.tsx
@@ -1,18 +1,33 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import FormExampleForm from "./Form";
+import {emailFormat} from "./validation"
+
 
 describe("Email Validation Test Case", () => {
 
-   
-    test(`Validate to don't allow any special characters in the customers email`, () => {
+    test(`Intitaly no Error Message is displayed`, () => {
       render(<FormExampleForm />);
       let test1: any = screen.getByTestId("submitTestId");
       fireEvent.click(test1);
-      let test2: any = screen.getByTestId("submitTestIdPara");
-      let test3 : any =test2.innerHTML;
-      expect(test3).toBe("Email is valid")
+      let test2: any = screen.getByTestId("submitTestIdPara").innerHTML;
+      expect(test2).toBe("")
     });
-  
-   
+
+    test("Should fail on non valid email Formate",()=>{
+      expect(emailFormat('Shubham.com')).toBe('Please enter your email in this format: name@company.com.')
+    })
+    test("Should not allow non valid email", () => {
+      expect(emailFormat('@gmail.com')).toBe("Please enter your email in this format: name@company.com.")
+    })
+    test("Should not allow special characters for email", () => {
+        expect(emailFormat('fàotlocker@gmail.com')).toBe("We don't allow any special characters in the customers email and ID.")
+    })
+    test("Should not allow special characters for email name", () => {
+        expect(emailFormat('àŒabc@gmàŒ.com')).toBe("We don't allow any special characters in the customers email and ID.")
+    })
+    test("Should allow on valid email", () => {
+        expect(emailFormat('footlocker@gmail.com')).toBeUndefined()
+    })
+
   });
   
